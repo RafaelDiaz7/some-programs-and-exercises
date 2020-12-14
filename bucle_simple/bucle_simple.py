18059e517e6159d57a3e42e7de31e31e2b536cf0
RafaelDiaz7 <rafaelaarondiaz123@gmail.com>
2020-12-14 15:41:56 -0400

    Programa que muestra en consola los números del 1 al 10 separados por un guión.

---
+++ bucle_simple/bucle_simple.py
@@ -0,0 +1,9 @@
+# Realizar un programa que utilizando una estructura repetitiva (bucle) presente por pantalla 
+# los números del 1 al 10 separados por un guión.
+# El resultado debe ser similar a esto:
+
+#    1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10
+
+
+for i in range(1,11):
+	print(i, end="-")
