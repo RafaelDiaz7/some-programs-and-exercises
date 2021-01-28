64f1becbba758abab58a3274dfccbe1a641cf7f3
RafaelDiaz7 <rafaelaarondiaz123@gmail.com>
2021-01-28 14:46:30 -0400

    Programa para contar cantidad de letras en go usando un map para evitar bucles for dobles.

---
+++ cantidad_letras/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	var demoText = "nananananan bbbbbbbbb...___7863,,,,'''"
+	//var demoText = "Lorem ipsum dolor sit amet consectetur adipiscing elit nisi, condimentum blandit elementum bibendum sed potenti accumsan faucibus ligula, euismod netus justo auctor luctus tincidunt gravida. At etiam litora massa parturient quis blandit class ridiculus taciti auctor molestie himenaeos potenti, a in neque dui praesent enim sem risus ac viverra dictumst cum, cubilia senectus et placerat vel quisque lacinia accumsan natoque aliquam semper tempus. Dapibus egestas elementum aliquet aliquam parturient euismod eu vulputate nulla magnis, non semper nam tempus commodo quam fames inceptos vitae, purus sapien augue suspendisse id hendrerit cum fermentum luctus.\n\nFringilla primis lectus sociis habitant tincidunt potenti tempus conubia, libero vivamus dictumst aliquet condimentum hac magnis, posuere viverra massa imperdiet nulla et purus. Viverra cursus mi lectus neque enim nisi pretium suscipit dis, scelerisque malesuada sollicitudin venenatis facilisi ultrices nulla massa. Integer platea hac vehicula commodo phasellus a varius, duis curabitur cras sociosqu nunc mollis torquent natoque, luctus euismod primis litora non lobortis."
+	demoText = strings.ReplaceAll(demoText, ".", "")
+	fmt.Println(demoText)
+	normalizedText := normalize(demoText)
+	fmt.Println(normalizedText)
+	fmt.Println(countLetters(normalizedText))
+}
+
+func normalize(text string) string {
+	re := regexp.MustCompile(`[_?!.,')(/;:1234567890 ]`)
+	return strings.ToLower(re.ReplaceAllString(text, ``))
+}
+
+func countLetters(text string) map[string]uint8 {
+	var letters = make(map[string]uint8)
+	for i := 0; i <= len(text)-1; i++ {
+		if letters[string(text[i])] >= 1 {
+			fmt.Println(letters[string(text[i])],string(text[i]))
+			letters[string(text[i])] += 1
+			fmt.Println(letters[string(text[i])],string(text[i]))
+		}
+		if letters[string(text[i])] == 0 {
+			letters[string(text[i])] = 1
+		}
+	}
+	return letters
+}
