7552b8b7b765ac005518a5279220c5b0de884673
Rafael Díaz <rafaelaarondiaz123@gmail.com>
2021-04-25 13:38:05 -0400

    Fixing typo in comment

--- binary-operators/main.go
+++ binary-operators/main.go
@@ -7,7 +7,7 @@ func main() {
 	// I want to add a number to 'a' uint8 variable using binary operators and hexadecimal numbers
 	a |= 0x10
 
-	/* The |= operator in this case means: a = a | 0x10. That statement means: assing to 'a' the bits that belong to either 'a' or 0x10 */ 
+	/* The |= operator in this case means: a = a | 0x10. That statement means: assign to 'a' the bits that belong to either 'a' or 0x10 */ 
 
 	//This would print 31 if the operation was succesfull
 	fmt.Println(a)
